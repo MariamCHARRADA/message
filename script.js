dfe28062e23fa65bfecd3f54b8f8ca86c251325c
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-31 22:06:11 +0100

    finalll

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   // Existing code to change question and gif
-  question.innerHTML = "Niahahahah 😈<br> you've just sealed your fate!<br> Brace yourself for another year<br> of my weirdness, marjjjness, and<br>baarcha bous w 3adh w taanig w 7ess";
+  question.innerHTML = "Niahahahah 😈<br> you've just sealed your fate!";
   gif.src = "https://media3.giphy.com/media/maPtLNbAsA0pRkozT9/giphy.gif?cid=ecf05e47oaapvfc34alqg0ip68ot9c3ge28vrlo1wlw3ek3n&ep=v1_gifs_related&rid=giphy.gif&ct=g";
 
 
   // Adding the new text dynamically under the question
   const additionalText = document.createElement('p');
   additionalText.className = 'additional-text';
-  additionalText.innerHTML = "Welcome to the chaos haboubii 😘<br> Kol aaam wenty l hob 😍 <br> weni l klab 😝 ahaha ";
+  additionalText.innerHTML = "Brace yourself for another year<br> of my weirdness, marjjjness, and<br>baarcha bous w 3adh w taanig w 7ess! <br> Welcome to the chaos haboubii 😘<br> Kol aaam wenty l hoob 😍 <br> weni l klab 😝 ahaha ";
 
   wrapper.appendChild(additionalText);
 });
