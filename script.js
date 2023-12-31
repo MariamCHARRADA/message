44ccbc2104f59bd948d5aec81c6293a9b5f29bea
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-31 21:11:27 +0100

    update

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   // Existing code to change question and gif
-  question.innerHTML = "Niahahahah😈!!<br> you've just sealed your fate!<br> Brace yourself for another year<br> of my weirdness, marjjjness,<br>and barchaa bous w 3adh w 7ess 😈";
+  question.innerHTML = "Niahahahah😈!!<br> you've just sealed your fate!<br> Brace yourself for another year<br> of my weirdness, marjjjness,<br>and barchaa bous w 3adh w 7ess";
   gif.src = "https://media3.giphy.com/media/maPtLNbAsA0pRkozT9/giphy.gif?cid=ecf05e47oaapvfc34alqg0ip68ot9c3ge28vrlo1wlw3ek3n&ep=v1_gifs_related&rid=giphy.gif&ct=g";
 
   // Make the gif smaller
-  gif.width = 200; // Set your desired width
-  gif.height = 200; // Set your desired height
+  gif.width = 100; // Set your desired width
+  gif.height = 100; // Set your desired height
 
   // Adding the new text dynamically
   const welcomeText = document.createElement('p');
   welcomeText.className = 'welcome-text';
   welcomeText.innerHTML = "Welcome to the chaos haboubi 😘💖";
 
-  // Insert the new paragraph element after the existing elements
-  wrapper.insertBefore(welcomeText, noBtn.nextSibling);
+  // Append the new paragraph element after the existing elements
+  wrapper.appendChild(welcomeText); // Use appendChild instead of insertBefore
 });
 
 noBtn.addEventListener("mouseover", () => {
